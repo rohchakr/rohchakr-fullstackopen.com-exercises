ea005dfdc96074b32e8130eda13693299a9dc997
Rohit Chakraborty <rohchakr@gmail.com>
2020-05-11 15:56:10 +0530

    Exercise 2.4: course contents step9

--- part2/course-contents/src/index.js
+++ part2/course-contents/src/index.js
@@ -11,10 +11,12 @@ const Content = ({parts}) => (
 
 const Total = ({parts}) => (
   <p>
-    Number of exercises {
+  <strong>  
+    Total of {
       // parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises
       parts.map(p => p.exercises).reduce((acc, curr) => acc + curr)
-    }
+    } exercises
+  </strong>
   </p>
 )
 
@@ -27,35 +29,54 @@ const Course = ({ course }) => (
 )
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-      <Course course={course} />
+      courses.map( course => <Course key={course.id} course={course} />)
   )
 }
 
